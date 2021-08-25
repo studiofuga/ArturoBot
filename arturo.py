6fc70373aeaad9d84027bee6d3311dddf6e5edaa
Federico Fuga <fuga@studiofuga.com>
2021-08-25 22:14:13 +0200

    Connect with Twitter. Connect, timeline commands added.

--- arturo.py
+++ arturo.py
@@ -15,9 +15,41 @@ class Arturo:
 
         self.dispatcher = self.updater.dispatcher
         self.dispatcher.add_handler(telegram.ext.CommandHandler('start', self.start))
+        self.dispatcher.add_handler(telegram.ext.CommandHandler('connect', self.connect))
+        self.dispatcher.add_handler(telegram.ext.CommandHandler('timeline', self.timeline))
+
+        self.twitter_avail = False
+
+    def _check_twitter(self,update : telegram.Update, context : telegram.ext.CallbackContext):
+        if not self.twitter_avail or self.twitter is None:
+            context.bot.send_message(chat_id=update.effective_chat.id,
+                                     text="Twitter non è connesso. Provo a connettermi.")
+            self.connect(update, context)
+            return False
+        return True
 
     def start(self,update : telegram.Update, context : telegram.ext.CallbackContext):
         context.bot.send_message(chat_id=update.effective_chat.id, text="Ciao sono Arturito, in che modo posso aiutarti?")
+
+    def connect(self,update : telegram.Update, context : telegram.ext.CallbackContext):
+        self.twitter_auth = tweepy.OAuthHandler(arturo_conf.TWITTER_API_KEY, arturo_conf.TWITTER_SEC_KEY)
+        self.twitter_auth.set_access_token(arturo_conf.TWITTER_USR_KEY, arturo_conf.TWITTER_USR_TOKEN)
+        self.twitter = tweepy.API(self.twitter_auth)
+
+        try:
+            self.twitter.verify_credentials()
+            context.bot.send_message(chat_id=update.effective_chat.id, text="Ok, sono autenticato su twitter")
+            self.twitter_avail=True
+        except:
+            context.bot.send_message(chat_id=update.effective_chat.id, text="Autenticazione su twitter fallita")
+
+    def timeline(self,update : telegram.Update, context : telegram.ext.CallbackContext):
+        if not self._check_twitter(update, context):
+            return
+        tweets = self.twitter.home_timeline(count=5)
+        for tweet in tweets:
+            context.bot.send_message(chat_id=update.effective_chat.id, text="From {}: {}".format(tweet.user.name, tweet.text))
+
 
     def run(self):
         self.updater.start_polling()
--- arturo_conf.sample.py
+++ arturo_conf.sample.py
@@ -1,1 +1,7 @@
 TG_TOKEN="Put Here your Telegram Token"
+TWITTER_API_KEY="Place here you Twitter API key (Consumer)"
+TWITTER_SEC_KEY="Place here you Twitter API Secret key (Consumer)"
+TWITTER_TOKEN="Place here you Twitter Bearer Token"
+
+TWITTER_USR_KEY="Your User Key"
+TWITTER_USR_TOKEN="Your User Token"
